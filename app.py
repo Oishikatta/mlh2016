be4f3a24c0f4abcfccbc0e729c77b31298410379
Corey Hunter <Oishikatta+github.com@regex.be>
2016-12-03 09:44:28 -0800

    add api call example

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import json
+import urllib
 
 from flask import Flask
 from flask import render_template
@@ -13,6 +15,13 @@ if len(STEAM_API_KEY) != 32:
     print "Invalid Steam API key. Create steam_apikey.txt before running the app."
     exit()
 
+def getSteamIdFromVanityUrl(vanityUrl):
+    API_CALL = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={0}&vanityurl={1}"
+    url = urllib.urlopen(API_CALL.format(STEAM_API_KEY, vanityUrl)).read()
+    print json.loads(url)
+
+getSteamIdFromVanityUrl("isitdead")
+
 @app.route("/")
 def hello():
     return render_template("index.html")
@@ -23,3 +32,4 @@ def githook():
 
 if __name__ == "__main__":
     app.run()
+
